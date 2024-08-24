457b59fb4c70a8353234cd090e7e685f546baf9b
vampwtry <bsquintana@ucompensar.edu.co>
2024-08-24 18:19:55 -0500

    plantilla spring

---
+++ src/main/java/com/remipics/remipics/RemipicsApplication.java
@@ -0,0 +1,13 @@
+package com.remipics.remipics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemipicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemipicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remipics/remipics/RemipicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remipics.remipics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemipicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
