d18ecf698832181ad8d1f8927875ec6ea902629d
vampwtry <bsquintana@ucompensar.edu.co>
2024-08-24 18:56:19 -0500

    conexion y modelo

---
+++ src/main/java/com/remipics/remipics/entities/Usuario.java
@@ -0,0 +1,20 @@
+package com.remipics.remipics.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table (name = "usuario")
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long usuario_id; //long es un entero
+    private String correo;
+    private String nombre;
+    private String apellido;
+    private String fecha_naci;
+    private String sexo;
+    private String contraseña;
+}
